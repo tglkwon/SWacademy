291e4d4aae7726c945bf1f008e5498affd47c67f
tglkwon <tglkwon@gmail.com>
2022-09-21 09:10:21 +0900

    220919 패치 config 추가

---
+++ Data collecting/config.py
@@ -0,0 +1,10 @@
+# 네이버 쿠키
+c = ('''
+SB_MODE plusdeal	www.naver.com	/	세션	15						Medium	
+NID_JKL	q/dtgTttHDuM7WRMnbvITnV5Pg/WjZPF7/f7VNUj1Qk=	.naver.com	/	세션	51		✓				Medium	
+NID_SES	AAABmna16m3vbX7mKa+6GebwtHMBtKyFbs6DTPkAV7fRoRQEzG48eiTNCRqDnn1Uc+47/XI9vMvPvhFssrVaylGGwJuU3lI63yO90vqKjoaaxOVRjpNwDVUuS3uVidv3i7Y5dd0hUL/BoEdJ56vew2ZKCk36OSjTYuNwOzgJiWOY9lOY1loG590Hf2uIe8uPeTNXRgYVOA3rrXPArPHaim3BZqovbKVn5RRccvW0cMO//mWMqlcWgKahCFOEHFknHVNY4wMmZri4r0zc/mp2cz9Rl+uiQR6BxLgyVXfFmLaxzhH3vIswuyeyK8oj/Dh687g4NK7rvfyQFSy+dVkFta0HTzJY4HWXexqFALyqNgm16u7aAo/CvoYfuQNkPFt95R4HyebGPbrLpIpiYzmZcgNQEYqb0sC5j/sPEIbXJeBxeOdw6HcqQCc41I/q9o7GmM4wLumnoD+8e36N8NeZGVViziYxnE3vIQz3nxA2sW4it52GoxLIFC4uilcFBQF9Nz5VKzy79nBCifTAz+puly3dQ1S40m3K3eRxqMHBhMT9oFau	.naver.com	/	세션	567						Medium	
+NID_AUT	JJ/fLLTzDZKJITdStrBPEgo7GaFCQAS8rkuyqq8w3NDI0oFXPUDjMgFMHJbPfgFL	.naver.com	/	세션	71	✓					Medium	
+nid_inf	1549667697	.naver.com	/	세션	17						Medium	
+NNB	ZB4I5EXDFUUWG	.naver.com	/	2023-10-25T03:06:09.095Z	16		✓	None			Medium	
+PM_CK_loc	99770fda1fc68c0bdfc0549212eba9e4fc87147f612fe73e94e10fa875e35850	www.naver.com	/	2022-09-21T03:04:58.817Z	73	✓					Medium	
+''').splitlines()
