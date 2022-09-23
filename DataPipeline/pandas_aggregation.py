0c78b7f6972c66e26d0418855bb9e106a89c4311
tglkwon <tglkwon@gmail.com>
2022-09-23 17:32:57 +0900

    220922 업로드

---
+++ DataPipeline/pandas_aggregation.py
@@ -0,0 +1,25 @@
+import seaborn as sns
+import pandas as pd
+import missingno
+
+mpg = sns.load_dataset('mpg')
+# mpg.missingno.matrix
+
+# missing data를 채우는 테크닉 impute
+# 평균, 중간값, 최빈값 + 추천 알고리즘에서 썼던 비슷한 데이터 값을 찾을 수도 있다.
+# mpg[mpg['horsepower'].isna()]
+
+mpg[mpg.origin == 'usa'].horsepower.describe()
+mpg.groupby('origin').mean()[['horsepower', 'weight']].plot.bar(stacked=True)
+mpg.groupby('origin')[['horsepower', 'weight']].mean()
+# 필요한 정보를 계산하는 것도 순서와 방법을 잘 찾아야 한다.
+
+# groupby를 구현하는 개념 - split, imply, combine 카테고리 별 특성을 찾는 방법
+
+temp = mpg.groupby('origin')[['horsepower', 'weight']]
+temp.agg(['mean', 'std'])
+temp.aggregate(['mean', 'std'])
+
+tips = sns.load_dataset('tips')
+tips.groupby(['smoker', 'day'], as_index=False).mean()
+# 전반적인 경향성을 미리 확인하기 좋다
